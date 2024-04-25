dfc59b4aeb021914a19e15d6d985f7690887813c
James Scipio McCaffrey <jsmccaffrey@gmail.com>
2024-04-25 11:03:36 -0700

    Add files via upload

---
+++ src/main/java/com/example/demo/entities/Cart.java
@@ -0,0 +1,50 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "carts")
+@Getter
+@Setter
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long id;
+
+    @Column(name = "order_tracking_number")
+    private String orderTrackingNumber;
+
+    @Column(name = "package_price")
+    private BigDecimal package_price;
+
+    @Column(name = "party_size")
+    private int party_size;
+
+    @Enumerated(EnumType.STRING)
+    private StatusType status;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private Date create_date;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private Date last_update;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private Set<CartItem> cartItems;
+}
---
+++ src/main/java/com/example/demo/entities/CartItem.java
@@ -0,0 +1,41 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "vacation_id")
+    private Vacation vacation;
+
+    @ManyToMany(mappedBy = "cartItems")
+    private Set<Excursion> excursions;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private Date createDate;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private Date lastUpdate;
+}
